a770ace1ad6a2ed94332b377746bd41df2dec3b0
Moaaz Sidat <moaaz.omer@gmail.com>
2015-01-17 15:59:34 -0500

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Moaaz Sidat on 2015-01-17.
+//  Copyright (c) 2015 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Moaaz Sidat on 2015-01-17.
+//  Copyright (c) 2015 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
